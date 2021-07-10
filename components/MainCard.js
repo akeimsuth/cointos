10a9a463cf3dc8a1734b588e15aadba20f2fe029
akeimsuth <akeim.sutherland@sitebix.com>
2021-07-10 04:55:52 -0500

    added api to get crypto data

--- components/MainCard.js
+++ components/MainCard.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { LinearGradient } from 'expo'
 import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import axios from 'axios';
 
 const CardContainer = styled.View`
     display: flex;
@@ -27,16 +28,34 @@ const Loader = styled.TouchableOpacity`
     justify-content: center;
     align-content: center;
 `;
-
+const convertToTwoDecimals = (value) => {
+    let money = parseFloat(value).toFixed(2);
+    return money
+}
 const MainCard = () => {
+    const [btc, setBtc] = useState();
+    var options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coins',
+        headers: {
+          'x-rapidapi-key': '0367e3ee2emsh580b20d7fb15013p151c40jsn70320c457a72',
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+        }
+      };
+      axios.request(options).then(function (response) {
+        //console.log(response.data.data.coins[0].price);
+        setBtc(response.data.data.coins[0].price);
+    }).catch(function (error) {
+        console.error(error);
+    });
     return (
         <CardContainer>
             <Image style={{position: 'absolute', width: 400, height: 150, borderRadius: 20}} source={{uri: "https://static.vecteezy.com/system/resources/previews/000/539/900/non_2x/abstract-gradient-background-vector.jpg"}} />
             <View style={styles.header}>
                 <Text style={styles.smallText}>$</Text>
-                <Text style={styles.mainText}>9238</Text>
+                <Text style={styles.mainText}>{convertToTwoDecimals(btc)}</Text>
                 <View style={{opacity:0.8, justifyContent:"flex-end"}}>
-                    <Text style={styles.smallText}>.31</Text>
+                    <Text style={styles.smallText}></Text>
                 </View>
             </View>
             <View style={{paddingTop:10}}>
